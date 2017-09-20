456f068284e43a590e3aea3f0676b1cc20652d8a
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2017-09-19 21:06:21 -0400

    2.0 skeleton

---
+++ Hockey Playoffs/AppController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class AppController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
