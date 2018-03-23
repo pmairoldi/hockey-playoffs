630d53461702cf2c5623733b368842a5416005f5
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2018-03-23 19:38:00 -0400

    update to support iPhone X

--- HockeyPlayoffs/View/BracketView.m
+++ HockeyPlayoffs/View/BracketView.m
@@ -25,7 +25,7 @@
     self = [super initWithFrame:frame collectionViewLayout:[[self class] getLayout]];
     
     if (self) {
-      
+        
         self.backgroundColor = [UIColor clearColor];
         self.autoresizesSubviews = YES;
         self.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
@@ -57,15 +57,15 @@
     
     switch (section) {
             
-            case west_quarter:
-            case east_quarter:
-
+        case west_quarter:
+        case east_quarter:
+            
             if (rows <= 1) {
                 interItemSpacing = 10.0;
             }
             
             else {
-             
+                
                 interItemSpacing = ((width - (rows * itemWidth)) / (rows - 1)) - (itemWidth / (rows - 1));
                 
                 if (interItemSpacing < 10.0) {
@@ -75,8 +75,8 @@
             
             break;
             
-            case west_semi:
-            case east_semi:
+        case west_semi:
+        case east_semi:
             
             if (rows <= 1) {
                 interItemSpacing = 10.0;
@@ -92,13 +92,13 @@
             
             break;
             
-            case west_final:
-            case east_final:
+        case west_final:
+        case east_final:
             
             interItemSpacing = 10.0;
             break;
             
-            case final:
+        case final:
             
             interItemSpacing = 10.0;
             break;
@@ -108,7 +108,7 @@
             interItemSpacing = 10.0;
             break;
     }
-
+    
     return interItemSpacing;
 }
 
@@ -120,8 +120,9 @@
 -(CGSize)layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
     
     int width = (self.frame.size.width - 2 * SIDE_MARGIN - [BracketModel numberOfRowsInSection:indexPath.section] * 10.0)/[BracketModel numberOfRowsInSection:indexPath.section];
-  
-    int height = (self.frame.size.height - TOP_MARGIN - BOTTOM_MARGIN - ([BracketModel numberOfSections] - 1) * MIN_SECTION_HEADER_HEIGHT)/[BracketModel numberOfSections];
+    
+    
+    int height = ([self getHeightForView: self] - TOP_MARGIN - BOTTOM_MARGIN - ([BracketModel numberOfSections] - 1) * MIN_SECTION_HEADER_HEIGHT)/[BracketModel numberOfSections];
     
     return CGSizeMake(MIN(width, 60.0), MIN(height, 55.0));
 }
@@ -129,7 +130,7 @@
 -(UIEdgeInsets)layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
     
     CGFloat itemWidth = [self layout:collectionViewLayout sizeForItemAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:section]].width;
-
+    
     NSInteger numberOfSections = [BracketModel numberOfSections];
     
     CGFloat topOffest;
@@ -155,16 +156,16 @@
     
     switch (section) {
             
-            case west_semi:
-            case east_semi:
-
+        case west_semi:
+        case east_semi:
+            
             marginOffset = itemWidth/2 + [self layout:collectionViewLayout minimumInteritemSpacingForSectionAtIndex:west_quarter]/MAX(1, [BracketModel numberOfRowsInSection:section]);
             break;
             
-            case west_final:
-            case east_final:
-            case final:
-
+        case west_final:
+        case east_final:
+        case final:
+            
             marginOffset = MAX((self.frame.size.width - 2 * SIDE_MARGIN - itemWidth)/2,[self layout:collectionViewLayout minimumInteritemSpacingForSectionAtIndex:west_semi]);
             break;
             
@@ -180,14 +181,14 @@
 -(CGSize)layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section {
     
     if (section > 0) {
-
+        
         NSInteger numberOfSections = [BracketModel numberOfSections];
-
-        CGFloat viewHeight = self.frame.size.height - TOOLBAR_HEIGHT - STATUS_BAR_HEIGHT - TOP_MARGIN - BOTTOM_MARGIN;
+        
+        CGFloat viewHeight = [self getHeightForView: self] - TOP_MARGIN - BOTTOM_MARGIN;
         CGFloat itemHeight = [self layout:collectionViewLayout sizeForItemAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:section]].height;
-
+        
         CGFloat lineSpacing = (viewHeight - (itemHeight * numberOfSections)) / (numberOfSections - 1);
-
+        
         return CGSizeMake(self.frame.size.width, ceilf(lineSpacing));
     }
     
@@ -201,11 +202,11 @@
     NSMutableArray *temp = [[NSMutableArray alloc] init];
     
     UIEdgeInsets edgeInset = [self layout:nil insetForSectionAtIndex:round];
-
+    
     CGSize itemSize = [self layout:nil sizeForItemAtIndexPath:[NSIndexPath indexPathWithIndex:0]];
     
     CGFloat minItemSpacing = [self layout:nil minimumInteritemSpacingForSectionAtIndex:round];
-
+    
     NSInteger numberOfItems = [BracketModel numberOfRowsInSection:round];
     
     CGFloat itemSpacing;
@@ -216,14 +217,14 @@
     }
     
     else {
-    
+        
         itemSpacing = MAX((self.frame.size.width - edgeInset.left - edgeInset.right - numberOfItems * itemSize.width)/MAX(1, numberOfItems-1), minItemSpacing);
     }
     
     for (int i = 0; i < numberOfItems; ++i) {
         
         CGFloat x;
-
+        
         if (numberOfItems == 1) {
             x = edgeInset.left + itemSpacing;
         }
@@ -244,13 +245,12 @@
     return temp;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
+-(CGFloat)getHeightForView:(UIView *)view {
+    if (@available(iOS 11.0, *)) {
+        return view.safeAreaLayoutGuide.layoutFrame.size.height;
+    } else {
+        return view.frame.size.height - TOOLBAR_HEIGHT - STATUS_BAR_HEIGHT;
+    }
 }
-*/
 
 @end
